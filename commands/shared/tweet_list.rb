22c4032e9f762a4fc12746670caa87208212388c
Gabriel Leal <gabrielthleal@gmail.com>
2020-08-17 20:39:17 -0300

    add new command to show tweet list

---
+++ commands/shared/tweet_list.rb
@@ -0,0 +1,36 @@
+require 'readline'
+
+module TweetConsole 
+def self.list_tweets(user_name=nil, is_me=false)
+  page = 1
+
+  while true 
+    tweets =
+      if is_me 
+        auth_client.user_timeline(page: page, count: 5, tweet_mode: 'extended')
+      else
+        auth_client.user_timeline(user_name, page: page, count: 5, tweet_mode: 'extended')
+      end
+    
+    tweets.each do |t|
+      puts ''
+
+      Printc.pair('Text', t.attrs[:full_text], :yellow, :white, 0.1)
+      Printc.pair('Number of retweets', t.retweet_count, :yellow, :white, 0.1)
+      puts ''
+
+      option = (
+        'What do you prefer?'
+        {
+          'Back to menu': :exit,
+          'See more tweets': :more_tweets
+        }
+      )
+
+      break if option == :exit 
+
+      page += 1 if :more_tweets
+      end
+    end
+  end
+end
