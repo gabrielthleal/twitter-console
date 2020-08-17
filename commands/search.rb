486a16a362e310abf1a479268838f260c0d1e47e
Gabriel Leal <gabrielthleal@gmail.com>
2020-08-17 14:06:09 -0300

    add options to files command search and help and search

--- commands/search.rb
+++ commands/search.rb
@@ -1,4 +1,29 @@
+require 'radline'
+
 module TweetConsole
-	def self.search
-	end
+  def self.search
+    while true
+      Printc.title('Which term do you want to search for?', :green, 1, true)
+      option = Readline.readline(">> ")
+
+      tweets = auth_client.search("#{option} -rt", count: 10, tweet_mode: 'extended', result_type: 'recent').take(10)
+      
+      tweets.each do |t|
+        puts ''
+        Printc.pair('Text', t.attrs[:full_text], :yellow, :white, 0.1)
+        Printc.pair('User', t.user.name, :yellow, :white, 0.1)
+        Printc.pair('Number of retweets', t.retwee_count, :yellow, :white, 0.1)
+      end
+
+      option = menu(
+        'What do you prefer?'
+        {
+          'Do another search': :search,
+          'Back to menu': :exit
+        }
+      )
+
+      break if option == :exit
+    end
+  end 
 end
