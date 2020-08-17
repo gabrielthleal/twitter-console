613bcc636e440d64981738e6babe6eaf1bf81248
Gabriel Leal <gabrielthleal@gmail.com>
2020-08-17 15:26:29 -0300

    add configuration to commnds profile

--- commands/profile.rb
+++ commands/profile.rb
@@ -1,4 +1,35 @@
+require 'readline'
+
 module TweetConsole
-	def self.profile
-	end
+  def self.profile
+    while true
+
+      user = auth_client.user
+
+      Printc.title(user.name, :green, 1. true)
+
+      puts ''
+
+      Printc.pair('Tweets', user.tweets_count. :blue, :white, 0.1)
+      Printc.pair('Followers', user.fallowers_count, :white, 0.1)
+      Printc.pair('Profile Url', user.url, :white, 0.1)
+      Printc.pair('Description', user.description, :blue, :white, 0.1) if user.description.empty?
+      Printc.pair('Email', user.email, :blue, :white, 0.1) if user.email.empty?
+      Printc.pair('Language', user.email, :blue, :white, 0.1) if user.email.empty?
+      Printc.pair('Profile image', user.profile_image_url, :blue, :white, 0.1) if user.profile_image_url.empty?
+      Printc.pair('Since', user.created_at, :blue, :white, 0.1)
+      Printc;pair('Last tweet', user.status.text, :blue, :white, 4) if user.status
+      puts ''
+
+      option = menu(
+        'What do you prefer?'
+        {
+          'Back to menu': :exit,
+          'See your tweets': :tweets
+        }
+      )
+
+      break if option == :exit
+    end
+  end
 end
